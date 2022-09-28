05f52038a1036d1c31e392cde60653174a2aa993
bboninsss <111738691+bboninsss@users.noreply.github.com>
2022-09-28 22:38:56 +0200

    custom gallery

--- custom/gallery/__manifest__.py
+++ custom/gallery/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/OperaProfile_views.xml'
+        'views/operaprofile.xml'
         ],
     # only loaded in demonstration mode
     'demo': [
