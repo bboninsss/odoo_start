58dfb6d8d3f5974fbf7302b0c1ba93a69a2bd71c
bboninsss <111738691+bboninsss@users.noreply.github.com>
2022-10-23 15:22:18 +0200

    mod

--- addons/galleria/models/artista.py
+++ addons/galleria/models/artista.py
@@ -10,11 +10,18 @@ class artista(models.Model):
     _recname="code"
 
     code=fields.Char(string="Codice Fiscale", required=True)
-    name=fields.Char(string="Nome")
     surname=fields.Char(string="Cognome")
+    name=fields.Char(string="Nome")
     place=fields.Char(string="Luogo di nascita")
     birth=fields.Date(string="Data di nascita")
     telefono=fields.Char(string="Telefono")
     email=fields.Char(string="E-mail")
     activity=fields.Boolean(string="In attività")
     iban=fields.Char(string="Iban")
+
+    def name_get(self):
+        result=[]
+        for rec in self:
+            name='' + rec.name + '  ' + rec.surname
+            result.append((rec.id, name))
+        return result 
